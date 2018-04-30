ea8952db63799a59fc2dcdef7626bd2caf802546
vismay-golwala <golwalavismay9@gmail.com>
2018-04-30 18:05:06 -0400

    Hello

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+	public static void main(String[] args) {
+		Main m = new Main();
+		//String s = "x = divide(1,2);\n";
+		//System.out.println(s.substring(4,s.length() - 2));
+	}
+
+
+	// $ public float divide(int a, int b) { $
+	public float divide(int a, int b) {
+		return (float) a / b;
+	}
+}
