4921c3bd60953ab2f289d766bdeaf3dfaf7274c3
vismay-golwala <golwalavismay9@gmail.com>
2018-05-05 17:16:06 -0400

    Changes to test

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,10 @@
 public class Main {
 	public static void main(String[] args) {
 		Main m = new Main();
-		//String s = "x = divide(1,2);\n";
-		//System.out.println(s.substring(4,s.length() - 2));
+		System.out.println(m.divide(10, 5));
+		
 	}
-
-
-	// $ public float divide(int a, int b) { $
+	
 	public float divide(int a, int b) {
 		return (float) a / b;
 	}
